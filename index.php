0028a2a7bd84662856c67c79a0ccd180364defb0
neto <neto@neto-lap>
2015-12-16 19:15:17 -0600

    subee

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<?php
-
-echo "Probando esta chiva";
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <header>
+            Este es un texto
+        </header>
+    </body>
+</html>
